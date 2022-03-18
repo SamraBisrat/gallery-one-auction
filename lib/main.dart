7751d8526bdbb2482902850d0e8d8b48b074b50c
SamraBisrat <samrakahsay@gmail.com>
2022-03-18 16:50:14 +0300

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,23 @@ import 'package:gallery_one_auction/Components/item_detail_page.dart';
 import 'package:gallery_one_auction/Components/login_page.dart';
 import 'package:gallery_one_auction/Components/profile_page.dart';
 import 'package:gallery_one_auction/Components/signup_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
+  
 }
 
+
+
+
 class MyApp extends StatelessWidget {
+  
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
@@ -17,4 +28,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(debugShowCheckedModeBanner: false, home: Signup());
   }
+  
+  
 }
